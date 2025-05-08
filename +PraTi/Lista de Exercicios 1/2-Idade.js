134e0b2da99e2c98532fa510c5e4ffee8e706392
Victor Costa <142239128+Viuutu@users.noreply.github.com>
2025-05-07 23:40:15 -0300

    Update 2-Idade.js

--- +PraTi/Lista de Exercicios 1/2-Idade.js
+++ +PraTi/Lista de Exercicios 1/2-Idade.js
@@ -1,16 +1,13 @@
-function classificarIdade(idade) {
-    if (idade >= 0 && idade <= 12) {
-      console.log("Criança");
-    } else if (idade >= 13 && idade <= 17) {
-      console.log("Adolescente");
-    } else if (idade >= 18 && idade <= 59) {
-      console.log("Adulto");
-    } else if (idade >= 60) {
-      console.log("Idoso");
-    } else {
-      console.log("Idade inválida");
-    }
-  }
-  
-  classificarIdade(25);
-  
+let idade = parseInt(prompt("Digite a sua idade:"));
+
+if (idade >= 0 && idade <= 12) {
+    console.log("Criança");
+} else if (idade >= 13 && idade <= 17) {
+    console.log("Adolescente");
+} else if (idade >= 18 && idade <= 59) {
+    console.log("Adulto");
+} else if (idade >= 60) {
+    console.log("Idoso");
+} else {
+    console.log("Idade inválida.");
+}
