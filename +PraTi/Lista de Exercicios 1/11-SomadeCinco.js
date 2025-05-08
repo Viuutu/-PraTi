bf88234588b674b015636ea6409ab1a2f20fb0cf
Victor Costa <142239128+Viuutu@users.noreply.github.com>
2025-05-07 23:43:53 -0300

    Update 11-SomadeCinco.js

--- +PraTi/Lista de Exercicios 1/11-SomadeCinco.js
+++ +PraTi/Lista de Exercicios 1/11-SomadeCinco.js
@@ -1,11 +1,8 @@
-function somarNumeros() {
-    let soma = 0;
-    for (let i = 0; i < 5; i++) {
-      let num = parseInt(prompt(`Digite o ${i+1}° número:`));
-      soma += num;
-    }
-    console.log(`Soma total: ${soma}`);
-  }
-  
-  somarNumeros();
-  
+let soma = 0;
+
+for (let i = 1; i <= 5; i++) {
+    let num = parseFloat(prompt(`Digite o ${i}º número:`));
+    soma += num;
+}
+
+console.log("Soma total:", soma);
