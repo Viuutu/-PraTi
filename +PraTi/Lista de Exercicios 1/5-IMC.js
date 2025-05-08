295727d278ef1dbc64b82239ca9c21a103b3b324
Victor Costa <142239128+Viuutu@users.noreply.github.com>
2025-05-07 23:41:50 -0300

    Update 5-IMC.js

--- +PraTi/Lista de Exercicios 1/5-IMC.js
+++ +PraTi/Lista de Exercicios 1/5-IMC.js
@@ -1,16 +1,15 @@
-function calcularIMC(peso, altura) {
-    let imc = peso / (altura * altura);
-  
-    if (imc < 18.5) {
-      console.log("Baixo peso");
-    } else if (imc >= 18.5 && imc < 24.9) {
-      console.log("Peso normal");
-    } else if (imc >= 25 && imc < 29.9) {
-      console.log("Sobrepeso");
-    } else {
-      console.log("Obesidade");
-    }
-  }
-  
-  calcularIMC(70, 1.75);
-  
+let peso = parseFloat(prompt("Digite o peso (kg):"));
+let altura = parseFloat(prompt("Digite a altura (m):"));
+
+let imc = peso / (altura * altura);
+console.log("IMC:", imc.toFixed(2));
+
+if (imc < 18.5) {
+    console.log("Baixo peso");
+} else if (imc < 25) {
+    console.log("Peso normal");
+} else if (imc < 30) {
+    console.log("Sobrepeso");
+} else {
+    console.log("Obesidade");
+}
