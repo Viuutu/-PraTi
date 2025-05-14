8eb0233fbc988c44b6b6e162b8daf87a9ee17dcb
Victor Costa <142239128+Viuutu@users.noreply.github.com>
2025-05-13 22:21:06 -0300

    Update 2.js

--- +PraTi/Lista de Exercicios 1/2.js
+++ +PraTi/Lista de Exercicios 1/2.js
@@ -1,6 +1,4 @@
-// Crie um programa que classifica a idade de uma pessoa em categorias (criança,
-// adolescente, adulto, idoso) com base no valor fornecido, utilizando uma estrutura de
-// controle if-else.
+// Crie um programa que classifica a idade de uma pessoa em categorias (criança, adolescente, adulto, idoso) com base no valor fornecido, utilizando uma estrutura de controle if-else.
 
 let idade = parseInt(prompt("Digite a sua idade:"));
 
