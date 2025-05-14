5ea44753ec454d530865e14e993fcc3457fd2795
Victor Costa <142239128+Viuutu@users.noreply.github.com>
2025-05-13 22:20:39 -0300

    Update 4.js

--- +PraTi/Lista de Exercicios 1/4.js
+++ +PraTi/Lista de Exercicios 1/4.js
@@ -1,3 +1,5 @@
+// 4. Crie um menu interativo no console que oferece ao usuário a escolha de três opções. Utilize switch-case para implementar a lógica de cada opção selecionada.
+
 let opcao = parseInt(prompt("Escolha uma opção:\n1 - Ver perfil\n2 - Editar perfil\n3 - Sair"));
 
 switch (opcao) {
