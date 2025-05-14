7aee96736b8bb5d4f064fc3bac6e69ba8190cf0c
Victor Costa <142239128+Viuutu@users.noreply.github.com>
2025-05-13 22:35:33 -0300

    Update 13.js

--- +PraTi/Lista de Exercicios 1/13.js
+++ +PraTi/Lista de Exercicios 1/13.js
@@ -1,17 +1,20 @@
-let soma = 0;
-let count = 0;
-let num;
+// 13. Fazer um algoritmo para receber números decimais até que o usuário digite 0 e fazer a média aritmética desses números.
+
+const prompt = require('prompt-sync')()
+let soma = 0
+let count = 0
+let num
 
 do {
-    num = parseFloat(prompt("Digite um número decimal (0 para sair):"));
+    num = parseFloat(prompt("Digite um número decimal (0 para sair):"))
     if (num !== 0) {
-        soma += num;
-        count++;
+        soma += num
+        count++
     }
-} while (num !== 0);
+} while (num !== 0)
 
 if (count > 0) {
-    console.log("Média:", (soma / count).toFixed(2));
+    console.log("Média:", (soma / count).toFixed(2))
 } else {
-    console.log("Nenhum número foi inserido.");
+    console.log("Nenhum número foi inserido.")
 }
