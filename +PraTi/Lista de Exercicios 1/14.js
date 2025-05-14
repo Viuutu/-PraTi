57653dc064aed5631c77fcf6b48528242f36c7a9
Victor Costa <142239128+Viuutu@users.noreply.github.com>
2025-05-13 22:37:17 -0300

    Update 14.js

--- +PraTi/Lista de Exercicios 1/14.js
+++ +PraTi/Lista de Exercicios 1/14.js
@@ -1,8 +1,11 @@
-let num = parseInt(prompt("Digite um número inteiro positivo:"));
-let fatorial = 1;
+// 14. Crie um programa que calcula o fatorial de um número fornecido pelo usuário utilizando um loop for ou while.
+
+const prompt = require('prompt-sync')()
+let num = parseInt(prompt("Digite um número inteiro positivo:"))
+let fatorial = 1
 
 for (let i = 1; i <= num; i++) {
-    fatorial *= i;
+    fatorial *= i
 }
 
-console.log(`Fatorial de ${num} é ${fatorial}`);
+console.log(`Fatorial de ${num} é ${fatorial}`)
