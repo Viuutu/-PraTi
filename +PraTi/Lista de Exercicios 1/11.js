4e32aaa7b2df5e7a4ae38674e6d38e0ee41fc43e
Victor Costa <142239128+Viuutu@users.noreply.github.com>
2025-05-13 22:31:38 -0300

    Update 11.js

--- +PraTi/Lista de Exercicios 1/11.js
+++ +PraTi/Lista de Exercicios 1/11.js
@@ -1,8 +1,12 @@
-let soma = 0;
+// 11. Escreva um programa que solicita ao usuário 5 números e calcula a soma total utilizando um loop for.
+
+
+const prompt = require('prompt-sync')()
+let soma = 0
 
 for (let i = 1; i <= 5; i++) {
-    let num = parseFloat(prompt(`Digite o ${i}º número:`));
-    soma += num;
+    let num = parseFloat(prompt(`Digite o ${i}º número:`))
+    soma += num
 }
 
-console.log("Soma total:", soma);
+console.log("Soma total:", soma)
